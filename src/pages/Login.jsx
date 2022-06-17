1a5d9b03548b9ac48db40876b7ef4b3c43baeadc
unchul <105141025+unchul@users.noreply.github.com>
2022-06-17 18:06:04 +0900

    1일차 회원가입 끝

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import apis from '../api/api'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const user = async () => {
+    await apis.addUser({
+      userName: '운철',
+      passWord: 1234
+    })
+  }
+
+
   return (
-    <div>Login</div>
+    <div>Login
+      <Link to='/'><div>homebutton</div></Link>
+      <button onClick={user}>등록</button>
+    </div>
   )
 }
 
