a8336fd89bad45e60b1bae48034e1d7f9b9df003
unchul <105141025+unchul@users.noreply.github.com>
2022-06-20 13:56:03 +0900

    4일차 post css 반쯤 완성

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createPostJson, loadPostJson } from "../redux/modules/post";
 import styled from "styled-components";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 
 const Post = () => {
     const dispatch = useDispatch();
@@ -20,7 +19,7 @@ const Post = () => {
 
     const postNew = async (e) => {
         e.preventDefault();
-            await apis
+        await apis
             .addPost({
                 contents: content,
             })
@@ -43,30 +42,64 @@ const Post = () => {
             });
     };
 
-    const textareaChange = (e) =>{
+    const textareaChange = (e) => {
         setContent(e.target.value.replaceAll("<br>", "\r\n"));
         setTextareaHeight(e.target.value.split('\n').length - 1);
     }
-    
+
     return (
         <>
             <Wrap>
-                <Inputbox
-                    type="text"
-                    placeholder="내용을 입력하세용"
-                    value={content}
-                    onChange={textareaChange}
-                    style={{ height: "100px" }}
-                    wrap="hard"
-                    cols="20"
-                />
+                <MessageForm>
+                    <div className='m_format'>
+                        <button>B</button>
+                        <button>I</button>
+                        <button>S</button>
+                        <span></span>
+                        <button>L</button>
+                        <span></span>
+                        <button>N</button>
+                        <button>D</button>
+                        <span></span>
+                        <button>Q</button>
+                        <span></span>
+                        <button>C</button>
+                        <button>C</button>
+                    </div>
+                    <div className='m_text'>
+                        <input
+                            type="text"
+                            placeholder="내용을 입력하세용"
+                            value={content}
+                            onChange={textareaChange}
+                            style={{ height: "100px" }}
+                            wrap="hard"
+                            cols="20"
+                        />
+                    </div>
+                    <div className='m_toolbar'>
+                        <button>+</button>
+                        <div className='buttons'>
+                            <span></span>
+                            <button>C</button>
+                            <button>A</button>
+                            <span></span>
+                            <button>E</button>
+                            <button>@</button>
+                            <button>F</button>
+                        </div>
+                        <div className='submit'>
+                            <UnderBar>
 
-                <UnderBar>
-                    <AiOutlinePlusCircle  />
-                    <Button2 onClick={postNew} type="submit">
-                        등록하기
-                    </Button2>
-                </UnderBar>
+                                <button onClick={postNew} type="submit">
+                                    보내기
+                                </button>
+
+                            </UnderBar>
+                        </div>
+                    </div>
+                </MessageForm>
+
             </Wrap>
         </>
     );
@@ -80,11 +113,54 @@ const Wrap = styled.div`
       color: black;
       margin: 15% auto;
       border: 1px white solid; */
-      width: 50%;
-      position: fixed;
-      bottom: 0
+      width: 95%;
+      margin: auto;
+      margin-top: 25px;
+      position: relative;
+      bottom: px;
+      overflow-x: hidden;
+
     `;
-const Inputbox = styled.textarea`
+
+
+const MessageForm = styled.div`
+
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: white;
+  button {
+    cursor: pointer;
+  }
+  .m_format {
+    padding: 4px;
+    background-color: #efefef;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    display: flex;
+    button {
+      height: 28px;
+      width: 28px;
+      padding: 2px;
+      margin: 2px;
+      border-radius: 4px;
+      border: none;
+      background-color: transparent;
+    }
+    span {
+      width: 1px;
+      height: 20px;
+      margin: 2px 4px;
+      background-color: #ccc;
+      align-self: center;
+    }
+  }
+  .m_text {
+    padding: 0px 10px;
+    box-sizing: border-box;
+    input {
+      border: none;
+      outline: none;
+
       display: flex;
       justify-content: center;
       align-items: center;
@@ -93,17 +169,87 @@ const Inputbox = styled.textarea`
       width: 90%;
       height: 30%;
       white-space: pre-line;
-    `;
+    }
+  }
+  .m_toolbar {
+    padding: 4px 6px 4px 6px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    button {
+      width: 24px;
+      height: 24px;
+      border-radius: 24px;
+      outline: none;
+      border: none;
+      font-size: 20px;
+    }
+    .buttons {
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+      // flex-shrink: 1;
+      // padding-left: 0;
+      // padding-right: 0;
+      span {
+        width: 1px;
+        height: 20px;
+        margin: 2px 4px;
+        background-color: #ccc;
+        align-self: center;
+      }
+      button {
+        font-size: 15px;
+        height: 28px;
+        width: 28px;
+        padding: 2px;
+        margin: 2px;
+        border-radius: 4px;
+        border: none;
+        background-color: transparent;
+
+      }
+    }
+    .submit {
+      button {
+        heigth: 28px;
+        border-radius: 4px;
+        padding: 2px 8px;
+        width: auto;
+        font-size: 13px;
+        background-color: #007a5a;
+        color: #fff;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+`;
+
+
+
+/* const Inputbox = styled.textarea`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin: 10px;
+      width: 90%;
+      height: 30%;
+      white-space: pre-line;
+    `; */
 const Button2 = styled.button`
       padding: 3px;
       margin-bottom: 20px;
     `;
- const UnderBar = styled.div`
+const UnderBar = styled.div`
     width: 90%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: row;
 `
-    
+
 export default Post;
